6f36dcef9c95a21e55640ae716c3685cca8c580e
gauravdewan <gauravdewan@live.com>
2024-08-28 19:03:38 +0100

    edit cors origins

--- app/fastAPI_config.py
+++ app/fastAPI_config.py
@@ -3,19 +3,11 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-from app import consts
-
-""" # Enable CORS based on environment
-if consts.ENV == "PROD":
-    allow_origins=["*"],  # Allow all origins
-else:
-    allow_origins=["*"],  # Allow all origins
-
-
+# Enable CORS for local development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allow_origins,
+    allow_origins=["http://localhost", "http://127.0.0.1", "http://localhost:3000", "https://front-tikroast.vercel.app", "https://front-tikroast.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-) """
+)
